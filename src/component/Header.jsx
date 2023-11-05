1126b0e9180c9a4a4a6c8305b043c11f064833c5
Mohamed-gp <mohamedterba6@gmail.com>
2023-11-05 08:52:00 +0100

    complete header with editing and add responsive to hero

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -4,6 +4,12 @@ import React, { useRef, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const Header = () => {
+    const header = useRef(null)
+    window.onscroll = () => {
+      scrollY > 5 ? header.current.classList.add("shadow-2xl","bg-[#120644]","box-shadow") : header.current.classList.remove("bg-red-500","shadow-2xl","bg-[#120644]","box-shadow")
+    }
+    // money type
+    const [money,setmoney] = useState("usd")
     const [toogle,settoogle] = useState(false)
     const menu = useRef(null)
 
@@ -20,10 +26,10 @@ const Header = () => {
     }  
 
     return (
-        <header className='text-white relative py-4'>
-            <div className="container flex p-4 justify-between items-center">
+        <header ref={header} className={`text-white sticky top-0 z-50 py-4 `}>
+            <div className="container flex p-4  justify-between items-center">
                 <Link to="/" className="self-start text-xl sm:text-3xl font-bold">BUY2COIN</Link>
-                <nav ref={menu} className='menu absolute w-full duration-1000 opacity-0 md:opacity-100  left-0 -top-48 text-black md:text-white px-10 py-5 md:p-0 bg-white md:bg-transparent md:w-auto md:static md:block'>
+                <nav ref={menu} className='z-30 menu absolute w-full duration-1000 opacity-0 md:opacity-100  left-0 -top-48 text-black md:text-white px-10 py-5 md:p-0 bg-white md:bg-transparent md:w-auto md:static md:block'>
                     <ul className='flex-col md:flex-row flex gap-5 text-lg sm:text-xl '>
                         <li>
                             <Link>Market</Link>
@@ -34,8 +40,8 @@ const Header = () => {
                     </ul>
                 </nav>
                 <select className='bg-[#26272b] focus:outline-none rounded-lg py-1 px-2'>
-                    <option className='h' value="usd">USD</option>
-                    <option className='' value="eur">EUR</option>
+                    <option className='h' value="usd" defaultValue={money == "usd"}>USD</option>
+                    <option className='' value="eur" defaultValue={money == "eur"}>EUR</option>
                 </select>
                 {toogle == false ? <FontAwesomeIcon onClick={(e) => {tooglehandler(e)}} icon={faBars} className='md:hidden cursor-pointer text-xl '/> : <FontAwesomeIcon onClick={(e) => {tooglehandler(e)}} icon={faX} className=' text-xl md:hidden cursor-pointer'/> }
             </div>
--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 
 const Hero = () => {
+
   return (
     <>
-    <div className="container p-4 relative">
-        <div className='flex flex-col'>
-            <p className='text-white'>TRACK AND TRADE</p>
-            <p className=''>CRYPTO CURRENCIES</p>
+      <div className="container p-4 relative my-6">
+        <div className='flex flex-col text-center'>
+          <p className=' text-white xl:text-7xl text-lg sm:text-xl md:text-3xl lg:text-5xl font-bold'>TRACK AND TRADE</p>
+          <p className='bg-gradient-to-b bg-transparent  md:text-4xl lg:text-6xl text-xl sm:text-2xl xl:text-8xl bg-clip-text  mt-2 from-[#2600fc] to-[#ff00ea] text-transparent
+  -webkit-background-clip: text"' >CRYPTO CURRENCIES</p>
         </div>
-    </div>
+        <div className="opacity-animation bitcoin-absolute absolute top-1/2 -translate-y-1/2 left-3">
+          <img src="/public/assets/imgs/bit-coin.png" alt="" className='w-16 sm:w-20 lg:w-28 xl:w-36' />
+        </div>
+        <div className="opacity-animation bitcoin-absolute absolute top-1/2 -translate-y-1/2 right-3">
+          <img src="/public/assets/imgs/eth-coin.png" alt="" className='w-16 sm:w-20 lg:w-28 xl:w-36' />
+        </div>
+
+      </div>
     </>
   )
 }
